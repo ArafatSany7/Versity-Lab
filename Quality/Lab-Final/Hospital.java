24738877dde9f987b5f1e6c53f7f8842c72abde1
ArafatSany7 <human.sany7@gmail.com>
2026-08-11 12:00:00 +0600

    Commit

---
+++ Quality/Lab-Final/Hospital.java
@@ -0,0 +1,23 @@
+//acces based on user pin ,login, rule
+//pin calculate by summing  the aasci value 3 login attept
+
+import java.util.Scanner;
+
+public class Hospital {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Pin");
+        int pin = sc.nextInt();
+
+        System.out.println(" Enter your number af attepmts");
+        int attempts = sc.nextInt();
+        System.out.println("Enter your Role");
+        String role = sc.next();
+
+        switch (role) {
+            case "Doctor":
+                if ()
+        }
+    }
+}
